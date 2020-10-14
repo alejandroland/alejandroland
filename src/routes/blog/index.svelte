effffefe6ed43be09d3befc1266954068620e747
Alex <ajf@fastmail.com>
2020-10-13 23:26:42 -0500

    UPDATE
    src/posts.js grabs the posts from /posts
    src/routes/[slug].svelte loads the post that src/routes/blog/index.svelte
    sends
    src/routes/blog/[slug].svelte does the same in case the post link
    points to blog/{permalink}

--- src/routes/blog/index.svelte
+++ src/routes/blog/index.svelte
@@ -1,13 +1,7 @@
 <script context="module">
-	export function preload() {
-		return this.fetch(`blog.json`).then(r => r.json()).then(posts => {
-			return { posts };
-		});
-	}
-</script>
-
-<script>
-	export let posts;
+	// export let posts;
+	// import the list of posts
+	import {posts} from '../../posts';
 </script>
 
 <style>
@@ -29,6 +23,6 @@
 				tell Sapper to load the data for the page as soon as
 				the user hovers over the link or taps it, instead of
 				waiting for the 'click' event -->
-		<li><a rel="prefetch" href="blog/{post.slug}">{post.title}</a></li>
+		<li><a rel="prefetch" href="{post.permalink}">{post.title}</a></li>
 	{/each}
 </ul>
